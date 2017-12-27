5dc3e73dd5cc8cd97d3212816f14087ca0c32328
¨IvanIlyich¨ <bushibukeyi@gmail.com>
2017-12-27 23:51:24 +0800

    561

---
+++ Array_Partition_I.go
@@ -0,0 +1,10 @@
+func arrayPairSum(nums []int) int {
+	var sum int
+	if !sort.IntsAreSorted(nums) {
+		sort.Ints(nums)
+	}
+	for i := 0; i <= len(nums); i += 2 {
+		sum += nums[i]
+	}
+	return sum
+}
